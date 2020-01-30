cd094fc265250259b422de2f190d1fa99213ffe8
newbi462 <isthisemailfree@gmail.com>
2020-01-30 16:21:59 -0500

    testing envirment set up

---
+++ index.spec.js
@@ -0,0 +1,11 @@
+//TEST ENDPOINTS?
+const request = require('supertest');
+
+const server = require('./index.js')
+
+describe('server', function() {
+    it('this test', function() {
+
+    })
+
+})
